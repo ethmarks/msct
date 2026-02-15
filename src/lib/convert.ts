8a89fce8ac1f65bac5c3c07dafb63d95cd0858b2
Ethan Marks <87040432+ethmarks@users.noreply.github.com>
2026-02-15 16:09:07 -0500

    add planckToCaesium()

--- src/lib/convert.ts
+++ src/lib/convert.ts
@@ -3,22 +3,16 @@ import {
   type Prefix,
   type PlanckQuantity,
   type Unit,
+  type ScaledQuantity,
+  type Dimensionality,
+  type MeasurementSystem,
   emptyPrefix,
   allPrefixes,
   allUnits,
-  allCustomaryUnits,
-  allCaesiumUnits,
 } from "./vocab.ts";
+import { assert } from "node:test";
 
 Decimal.set({ precision: 70 });
-
-export function stringToDecimal(input: string): Decimal | undefined {
-  try {
-    return Decimal(input);
-  } catch {
-    return undefined;
-  }
-}
 
 export function lookupPrefix(prefixId: string): Prefix | undefined {
   for (const candidate of allPrefixes) {
@@ -41,28 +35,85 @@ export function lookupUnit(unitName: string): Unit | undefined {
   }
 }
 
+export function assertDimensionality(
+  stringToCheck: String,
+  dimensionalityToAssert: Dimensionality,
+) {
+  if (stringToCheck !== dimensionalityToAssert) {
+    throw new Error(
+      `wrong dimensionality! Expected ${dimensionalityToAssert}, got: ${stringToCheck}`,
+    );
+  }
+}
+
+export function assertMeasurementSystem(
+  stringToCheck: String,
+  measurementSystemToAssert: MeasurementSystem,
+) {
+  if (stringToCheck !== measurementSystemToAssert) {
+    throw new Error(
+      `wrong measurement system! Expected ${measurementSystemToAssert}, got: ${stringToCheck}`,
+    );
+  }
+}
+
 /**
  * Calculates the number of Planck units in a given quantity by multiplying
  * the coefficient, prefix magnitude, and unit value
  *
- * @param coeff - the input coefficient
- * @param prefix - the input prefix
- * @param unit - the input unit
+ * @param input - ScaledQuantity
  * @returns a Decimal representing the number of Planck units in the quantity
  */
-export function getPlanck({
-  coeff,
-  prefix,
-  unit,
-}: {
-  coeff: Decimal;
-  prefix: Prefix;
-  unit: Unit;
-}): PlanckQuantity {
-  const adjustedCoeff = coeff.times(prefix.magnitude);
-  const planckValue = adjustedCoeff.times(unit.planck.dimensionality);
+export function getPlanck(input: ScaledQuantity): PlanckQuantity {
+  const adjustedCoeff = input.coeff.times(input.prefix.magnitude);
+  const planckValue = adjustedCoeff.times(input.unit.planck.value);
   return {
     value: planckValue,
-    dimensionality: unit.planck.dimensionality,
+    dimensionality: input.unit.planck.dimensionality,
+  };
+}
+
+export function planckToCaesium(
+  inputPlanck: PlanckQuantity,
+  targetCaesiumUnit: Unit,
+  addPrefixIfPossible: boolean,
+): ScaledQuantity {
+  // validate inputs
+  assertDimensionality(inputPlanck.dimensionality, "time");
+  assertDimensionality(targetCaesiumUnit.planck.dimensionality, "time");
+  assertMeasurementSystem(targetCaesiumUnit.system, "caesium");
+
+  const inputPlanckValue = inputPlanck.value;
+  const targetPlanckValue = targetCaesiumUnit.planck.value;
+
+  const unprefixedCoeff = inputPlanckValue.dividedBy(targetPlanckValue);
+
+  let coeff = unprefixedCoeff;
+  let prefix = emptyPrefix;
+
+  if (addPrefixIfPossible && targetCaesiumUnit.takesPrefixes) {
+    const unprefixedCoeffMagnitude = unprefixedCoeff.logarithm().floor();
+
+    const bestPrefix: Prefix = allPrefixes.reduce(
+      (best, candidate) => {
+        const distance = unprefixedCoeffMagnitude
+          .minus(candidate.magnitude.logarithm().floor())
+          .abs();
+
+        return distance.lessThan(best.distance)
+          ? { prefix: candidate, distance }
+          : best;
+      },
+      { prefix: emptyPrefix, distance: Decimal(Infinity) },
+    ).prefix;
+
+    prefix = bestPrefix;
+    coeff = unprefixedCoeff.dividedBy(prefix.magnitude);
+  }
+
+  return {
+    coeff,
+    prefix,
+    unit: targetCaesiumUnit,
   };
 }
